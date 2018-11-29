f7becba20c4f540fd9818b977d420cc618b5e69a
ArundaVictor <victoryugi31@gmail.com>
2018-11-29 22:58:01 +0300

    add function to script

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function triangle(a, b, c) {
+
+};
+var a = parseInt(prompt("Enter the value of side a"));
+var b = parseInt(prompt("Enter the value of side b"));
+var c = parseInt(prompt("Enter the value of side c"));
+var message = "The triangle comes up to " + a + b + c;
+if ((a === b && a === c & b === c)) {
+  alert("The triangle is an Equilateral triangle")
+} else if ((a !== b && a !== c && b !== c)) {
+  alert("The triangle is a Scalene triangle")
+} else if ((a === b || a === c || b === c)) {
+  alert("The triangle is an isoceles triangle")
+} else {
+  alert("That is not a triangle")
+}
