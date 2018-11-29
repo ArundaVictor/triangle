6a7e86e7db50792f97983a1b51183192cc2a5f25
ArundaVictor <victoryugi31@gmail.com>
2018-11-29 23:01:18 +0300

    Add variables to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function triangle(a, b, c) {
 var a = parseInt(prompt("Enter the value of side a"));
 var b = parseInt(prompt("Enter the value of side b"));
 var c = parseInt(prompt("Enter the value of side c"));
-var message = "The triangle comes up to " + a + b + c;
+
 if ((a === b && a === c & b === c)) {
   alert("The triangle is an Equilateral triangle")
 } else if ((a !== b && a !== c && b !== c)) {
