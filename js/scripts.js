7f9020f6ed3bd362c4ae355737bca4d5092679b4
ArundaVictor <victoryugi31@gmail.com>
2018-12-03 12:55:44 +0300

    make changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 var sides = []
+
 function triangle(sides) {
-var a = parseInt(document.getElementById("sideA").value);
-var b = parseInt(document.getElementById("sideB").value);
-var c = parseInt(document.getElementById("sideC").value);
-var sides = [];
-sides.push(a, b, c);
-var e = sides[0]
-var f = sides[1]
-var g = sides[2]
-if ((e===f && e===g && f===g)) {
-  alert("The triangle is an Equilateral triangle")
-} else if ((e!==f && e!==g && f!==g)) {
-  alert("The triangle is a Scalene triangle")
-} else if ((e==f || e==g || f==g)) {
-  alert("The triangle is an isoceles triangle")
-} else {
-  alert("That is not a triangle");
-}
+  var a = parseInt(document.getElementById("sideA").value);
+  var b = parseInt(document.getElementById("sideB").value);
+  var c = parseInt(document.getElementById("sideC").value);
+  var sides = [];
+  sides.push(a, b, c);
+  var e = sides[0]
+  var f = sides[1]
+  var g = sides[2]
+  if ((e === f && e === g && f === g)) {
+    alert("The triangle is an Equilateral triangle")
+  } else if ((e !== f && e !== g && f !== g)) {
+    alert("The triangle is a Scalene triangle")
+  } else if ((e == f || e == g || f == g)) {
+    alert("The triangle is an isoceles triangle")
+  } else {
+    alert("That is not a triangle");
+  }
 }
