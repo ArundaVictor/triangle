e6438b0c2dd6c3d6a5c9484a75d2f48a74de665a
ArundaVictor <victoryugi31@gmail.com>
2018-12-04 16:14:39 +0300

    make new changes

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,13 @@ function triangle(sides) {
   var e = sides[0]
   var f = sides[1]
   var g = sides[2]
+
   if ((e === f && e === g && f === g)) {
     alert("The triangle is an Equilateral triangle")
-  } else if ((e !== f && e !== g && f !== g)) {
-    alert("The triangle is a Scalene triangle")
   } else if ((e == f || e == g || f == g)) {
     alert("The triangle is an isoceles triangle")
+  } else if ((e+f>=g && e+g>=f && f+g>=e)) {
+    alert("The triangle is a Scalene triangle")
   } else if ((e+f<=g || e+g<=f || g+f<=e))
-    alert("That is not a triangle");
+    alert("That is not a triangle")
   }
-}
